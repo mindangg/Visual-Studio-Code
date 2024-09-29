d7501db58f7aaf74ec7e0cd3e270e5bed6dbdcbb
_.mindang <minhdang875425@gmail.com>
2024-09-29 12:15:06 +0700

    push

--- Website/Web Manga/js/main.js
+++ Website/Web Manga/js/main.js
@@ -1,3 +1,5 @@
+// popup
+
 document.getElementById("search_icon").addEventListener("click", function(){
     document.querySelector(".search_popup").style.display = "flex";
 })
@@ -13,3 +15,100 @@ document.getElementById("search_close").addEventListener("click", function(){
 document.getElementById("shopping_close").addEventListener("click", function(){
     document.querySelector(".shopping_popup").style.display = "none";
 })
+
+// let search_icon = document.getElementById("search_icon");
+// search_icon.onclick = function()
+// {
+//     document.querySelector(".search_popup").style.display = "flex";
+// }
+
+// let shopping_icon = document.getElementById("shopping_icon");
+// shopping_icon.onclick = function()
+// {
+//     document.querySelector(".shopping_popup").style.display = "flex";
+// }
+
+// let search_close = document.getElementById("search_close");
+// search_close.onclick = function()
+// {
+//     document.querySelector(".search_popup").style.display = "none";
+// }
+
+// let shopping_close = document.getElementById("shopping_close");
+// shopping_close.onclick = function()
+// {
+//     document.querySelector(".shopping_popup").style.display = "none";
+// }
+
+//slider
+
+let list = document.querySelector(".slider .list");
+let items = document.querySelectorAll(".slider .list .item");
+let dots = document.querySelectorAll(".slider .dots li");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function()
+{
+    if(active + 1 > lengthItems)
+        active = 0;
+    else
+        active++;
+    ReloadSlider();
+}
+
+prev.onclick = function()
+{
+    if(active - 1 < 0)
+        active = lengthItems;
+    else
+        active--;
+    ReloadSlider();
+}
+
+let refreshSlider = setInterval(() => {next.click()}, 4000);
+
+function ReloadSlider()
+{
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + "px";
+
+    let lastActiveDot = document.querySelector(".slider .dots li.active");
+    lastActiveDot.classList.remove("active");
+    dots[active].classList.add("active");
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => {next.click()}, 4000);
+}
+
+dots.forEach((li, key) =>{
+    li.addEventListener("click", function(){
+        active = key;
+        ReloadSlider();
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
